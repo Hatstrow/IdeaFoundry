8f65a640e5353e368d201464d36092cb59922d5b
Hatstrow <Hatstrow@users.noreply.github.com>
2019-10-28 12:45:35 -0400

    Update TraficlightControl.py
    setting up the structure

--- TraficlightControl.py
+++ TraficlightControl.py
@@ -1,1 +1,20 @@
-TraficlightControl.py
+import requests, datetime, os
+from bs4 import BeautifulSoup
+
+
+class Schedule(self):
+	'''this will first determine if we have a recent 
+	version of the schedule. This will update maybe only once a year. 
+	This will help reduce the amount of requests made to the website'''
+
+class LightSwitch(self):
+	'''this will control what mode the light is in
+	Mode 1: Game day lights
+	Mode 2: Random light show
+	Mode 3: All on'''
+
+class SiteScraper(self):	
+	'''this will just get the raw information from website'''
+
+
+class ()
