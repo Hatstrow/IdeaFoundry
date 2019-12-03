1721a5efdc38e2041d95f0ff8b33a6365b624207
Hatstrow <Hatstrow-home@users.noreply.github.com>
2019-12-02 21:05:59 -0500

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,30 @@
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network, time
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'HouseOfStone 2.4'
+password = 'houseofstone12'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+led = Pin(5, Pin.OUT)
+led2= Pin(2, Pin.OUT)
