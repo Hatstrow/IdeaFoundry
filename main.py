d5bba5213d9474e9daa6c03e14a3447bb3095412
Hatstrow <Hatstrow@users.noreply.github.com>
2019-12-03 12:44:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 # Complete project details at https://RandomNerdTutorials.com
-import time
-import TrafficTest
-for i in range(4):
-    led.value(0)
-    time.sleep(1)
-    print('led:' + str(i))
-    led.value(1)
+import time,
+import LightShow as ls
+
+LightShow = ls.LightShow(0,4,5)#initiate lightshow class with pin GPIO 0,4, and 5
 
 print('in main')
 HOST = '192.168.1.10'
@@ -15,21 +12,22 @@ while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST,PORT))
     msg = s.recv(8)
-    if len(msg) <=0:
+    if len(msg) <=0:#continue/start at top of loop if no message
       continue
   except:
     continue
-  if msg == 'False':
+  if str(msg) == 'False': # trying to be able to get out of this loop if message sent == False and "break" the loop
     break
+    #if message >0 loop will reach this point and do stuff dependent on whats in the message
+
   print(msg.decode('utf-8'))
-  TrafficTest.all(0,4,5)
-  # for i in range(10):
-  #   led.value(0)
-  #   led2.value(0)
-  #   time.sleep(1)
-  #   print('led:' + str(i))
-  #   led.value(1)
-  #   led2.value(1)
-  #   time.sleep(1)
+  if str(msg) == "1":
+    LightShow.GoodGuys1()
+  elif str(msg) == "2":
+    LightShow.BadGuys1()
+  else
+    continue
+  
+  
 
 
