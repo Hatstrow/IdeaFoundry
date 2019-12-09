cb2cecea81c1cbcb3d99b147f571486d57b9c747
Hatstrow <Hatstrow-home@users.noreply.github.com>
2019-12-08 20:34:23 -0500

    Update LightShow.py

--- LightShow.py
+++ LightShow.py
@@ -2,13 +2,19 @@ from machine import Pin
 import time
 class LightShow():
 	def __init__(self, pin1,pin2,pin3):
-		led1 = Pin(pin1, Pin.OUT)
-		led2 = Pin(pin2, Pin.OUT)
-		led3 = Pin(pin3, Pin.OUT)
+		self.led1 = Pin(pin1, Pin.OUT)
+		self.led1.value(1)
+		self.led2 = Pin(pin2, Pin.OUT)
+		self.led2.value(1)
+		self.led3 = Pin(pin3, Pin.OUT)
+		self.led3.value(1)
+		print(self.led1.value())
+		print(self.led2.value()) 
+		print(self.led3.value())  
 
-	def GoodGuys1():
+	def GoodGuys1(self):
 		time.sleep(2)
-		for i in range(10):
+		for i in range(3):
 				self.led1.value(0)
 				self.led2.value(1)
 				self.led3.value(1)
@@ -35,6 +41,17 @@ class LightShow():
 				time.sleep(.25)
 				for i in range(5):
 					time.sleep(.1)
-					self.led2.value(0)
+					self.led3.value(0)
 					time.sleep(.1)
-					self.led2.value(1)
+					self.led3.value(1)
+
+	def BadGuys1(self):
+		time.sleep(2)
+		self.led1.value(1)
+		self.led2.value(1)
+		self.led3.value(1)
+		for i in range(5):
+			time.sleep(.5)
+			self.led1.value(0)
+			time.sleep(.5)
+			self.led1.value(1)
